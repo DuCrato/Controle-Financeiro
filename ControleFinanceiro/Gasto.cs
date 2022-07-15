ea1a3e1c1fdcdb15a3fe655cb6e6a01b476358b3
DuCrato <williamanderson1994@hotmail.com>
2022-07-15 14:35:29 -0300

    first commit

---
+++ ControleFinanceiro/Gasto.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System;
+
+
+namespace ControleFinanceiro
+{
+    class Gasto : INotifyPropertyChanged
+    {
+        string title,
+               amountSpent,
+               description;
+
+        DateTime date;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Title
+        {
+            get => title;
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string AmountSpent
+        {
+            get => amountSpent;
+            set
+            {
+                if (amountSpent != value)
+                {
+                    amountSpent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => description;
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                if (date != value)
+                {
+                    date = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ ControleFinanceiro/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System;
+
+namespace ControleFinanceiro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Gasto> Gastos;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Gastos = new ObservableCollection<Gasto>()
+            {
+                new Gasto(){Title = "arroz",   AmountSpent = "4.85",  Date = DateTime.Now},
+                new Gasto(){Title = "feijão",  AmountSpent = "12,75", Date = DateTime.Now},
+                new Gasto(){Title = "farinha", AmountSpent = "2,50",  Date = DateTime.Now}
+
+            };
+            listGastos.ItemsSource = Gastos;
+        }
+    }
+}
